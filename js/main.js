d06ff5f9d3734f1d673484d718568442f300835e
Hafsa <ali.hafsa.ahmed@gmail.com>
2024-03-15 18:48:14 -0500

    WIP: basic updates to js,css,html

---
+++ js/main.js
@@ -0,0 +1,29 @@
+	/*----- constants -----*/
+const WORD_LOOKUP = {
+    "1": "",
+    "2": "",
+    "3": "",
+    "4": ""
+};
+const KEYWORD_LOOKUP = {
+    a: "",
+    b: "",
+    c: "",
+    d: "",
+    e: ""
+}
+
+	/*----- state variables -----*/
+let results; // tracking what being submitted and if it matches keywords
+let winner; // track if they are winning or not
+let hint;   // want to track how many hints they use up
+
+
+
+	/*----- cached elements  -----*/
+
+
+	/*----- event listeners -----*/
+
+
+	/*----- functions -----*/
