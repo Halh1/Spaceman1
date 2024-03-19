8ac4e2a0cc7cf4592a90ff2d0181c7e336ead51a
Hafsa <ali.hafsa.ahmed@gmail.com>
2024-03-19 14:14:43 -0500

    WIP: able to display my keyword subsitute

--- js/main.js
+++ js/main.js
@@ -1,27 +1,15 @@
 	/*----- constants -----*/
-const KEYWORD_LOOKUP = [
-    "Mytery Machine",
-    "Hex Girls",
-    "Scrappy Doo",
-    "Ghost Clown",
-    "Where Are You"
-]
-const HINT_LOOKUP ={
-    a: "The van",
-    b: "The girl Band",
-    c: "Mini Scooby Doo",
-    d: "The circus villan",
-    e: "Famous 3 words"
+const KEYWORD_LOOKUP = ["Mytery Machine", "Hex Girls", "Scrappy Doo", "Ghost Clown", "Where Are You"]
+const HINT_LOOKUP = ["The van", "The girl Band", "Mini Scooby Doo", "The circus villan", "Famous 3 words"];
 
-
-}
 const tries = 6;
 
 	/*----- state variables -----*/
-let whatIsEntered = []; //tracking what is being entered?
-let correctLetters = " "; // track if they are winning or not
-let wrongLetters= []; // tracking letters that are wrong
-let hint = {};   // want to track how many hints they use up
+let entered; //tracking what is being entered?
+let correctLetters; // track if they are winning or not
+let wrongLetters; // tracking letters that are wrong
+let hint;   // want to track how many hints they use up
+let winner; //tracking winnings 
 
 
 
@@ -29,11 +17,11 @@ let hint = {};   // want to track how many hints they use up
 	/*----- cached elements  -----*/
 let imgEl = document.getElementById("body");
 let messageEl = document.querySelector("h1");
-let displayWord = document.getElementById("display");
+//let displayWord = document.getElementById("display");
 // cache the hint and restart button or just only have them as event listners
 
 	/*----- event listeners -----*/
-let letterEl = document.getElementById("letter").addEventListener("click", handleClick);
+let letterEl = document.querySelector(".letters").addEventListener("click", handleClick);
 
 	/*----- functions -----*/
 
@@ -41,28 +29,69 @@ init();
 //initalizing all state
 
 function init() {
+    correctLetters = "" // should I randomize the hint and the keywords together?
+    displayWord = [];
+    console.log(correctLetters, displayWord);
+    wrongLetters = [];
+    hint = []; //Trying to find away to connect the randomization of correctLetters with it's matching hint.
+    render();
+}
+
+function randomizeWord(){
     correctLetters = KEYWORD_LOOKUP[Math.floor(Math.random() * KEYWORD_LOOKUP.length)].split('');
-    displayWord = correctLetters.map((letter)=> letter === "" ? "" : "_");
-    wrongLetters= [];
-    hint = {};
+}
 
+function chosenWord() {
+    displayWord =  correctLetters.map((letter)=> letter === "" ? "" : "_");
+    document.getElementById("display").innerHTML = displayWord;
+
+}
+
+randomizeWord();
+chosenWord();
+//function checkRightLetters() 
+
+function handleClick(event) {
+    let key;
+    key = event.target.textContent;
+    if (event.target.tagName == "BUTTON") return;
     render();
 }
 
-function optionWord(){
-    whatIsEntered = correctLetters.split("").map()
-}
 
-function handleClick(evt) {
-    if (evt.target.tagName == "BUTTTON") {
-        let key = evt.target.textContent;
-        } else {
-            whatIsEntered.value +=key;
-    }
-    render();
-}
  function render() {
     //renderRightWord();
     //renderCorrect();
 }
 
+/*if (evt.target.tagName == "BUTTTON") {
+    let key = evt.target.textContent;
+    } else {
+        whatIsEntered.value +=key;
+
+
+checking if its correct:
+        if (correctLetters.includes(key)) {
+        correctLetters.forEach((char, idx)=> {
+            if (char === key) displayWord[idx] = key;
+        })
+        }else {
+            wrongLetters.push(key);
+     }console.log(key);
+    render();
+}*/
+
+
+
+
+/*
+redoing init real quick?
+function init() {
+    correctLetters = KEYWORD_LOOKUP[Math.floor(Math.random() * KEYWORD_LOOKUP.length)].split(''); // should I randomize the hint and the keywords together?
+    displayWord = correctLetters.map((letter)=> letter === "" ? "" : "_");
+    console.log(correctLetters, displayWord);
+    wrongLetters = [];
+    hint = []; //Trying to find away to connect the randomization of correctLetters with it's matching hint.
+    render();
+}
+*/
