e5e12454b1ea21c3073ba19fc5b91c95954eaddb
Hafsa <ali.hafsa.ahmed@gmail.com>
2024-03-20 18:38:02 -0500

    WIP: Check if letters match or not

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 	/*----- constants -----*/
-const KEYWORD_LOOKUP = ["Mytery Machine", "Hex Girls", "Scrappy Doo", "Ghost Clown", "Where Are You"]
+const KEYWORD_LOOKUP = ["MYSTERY MACHINE", "HEX GIRLS", "SCRAPPY DOO", "GHOST CLOWN", "WHERE ARE YOU"]
 const HINT_LOOKUP = ["The van", "The girl Band", "Mini Scooby Doo", "The circus villan", "Famous 3 words"];
 
 const tries = 6;
@@ -8,77 +8,61 @@ const tries = 6;
 let entered; //tracking what is being entered?
 let correctLetters; // track if they are winning or not
 let wrongLetters; // tracking letters that are wrong
-let hint;   // want to track how many hints they use up
+  // want to track how many hints they use up
 let winner; //tracking winnings 
-
 
 
 
 	/*----- cached elements  -----*/
 let imgEl = document.getElementById("body");
-let messageEl = document.querySelector("h1");
+let messageEl = document.getElementById("status");
 let displayWord = document.getElementById("display");
-let letterEl = document.querySelector(".letters");
+let letterEl = document.getElementById("keyboard");
 let restart = document.getElementById("reset")
-// cache the hint and restart button or just only have them as event listners
+let hintEl = document.getElementById("Hints-list");
+// cache the hint button or just only have them as event listners
 
 	/*----- event listeners -----*/
 letterEl.addEventListener("click", handleClick);
 restart.addEventListener("click", init);
+//hintEl.addEventListener("click", )
 
 	/*----- functions -----*/
 
 init();
-//initalizing all state
+
 
 function init() {
     correctLetters = KEYWORD_LOOKUP[Math.floor(Math.random() * KEYWORD_LOOKUP.length)].split(''); // should I randomize the hint and the keywords together?
-    displayWord.innerHTML = correctLetters.map((letter)=> letter === "" ? "" : "_").join(" ");
+    displayWord.innerText = correctLetters.map((letter)=> letter === "" ? "" : "_").join(" ");
     wrongLetters = [];
     hint = []; //Trying to find away to connect the randomization of correctLetters with it's matching hint.
+    messageEl.innerText = "Guess!";
     render();
 }
 
 
-
-function checkRightLetters() {
-    if(correctLetters.includes(key)) return;
-}
 
 function handleClick(event) {
-    let key;
-    if (event.target.tagName == "BUTTON") {
-        key = event.target.textContent;
-    }
+    const key = event.target.textContent
+   // if (event.target.tagName === "BUTTON");
+    //console.log(key);
+    if(correctLetters.includes(key)) {
+        correctLetters.forEach((char, idx) => {
+            if (char === key) displayWord[idx]= key;
+        });
+        } else {
+            wrongLetters.push(key);
+        }  
+        alert(correctLetters); 
+        alert(wrongLetters);
     render();
+   
 }
 
+
+ 
 
  function render() {
-    //renderRightWord();
-    //renderCorrect();
+
 }
-
-/*if (evt.target.tagName == "BUTTTON") {
-    let key = evt.target.textContent;
-    } else {
-        whatIsEntered.value +=key;
-
-
-checking if its correct:
-        if (correctLetters.includes(key)) {
-        correctLetters.forEach((char, idx)=> {
-            if (char === key) displayWord[idx] = key;
-        })
-        }else {
-            wrongLetters.push(key);
-     }console.log(key);
-    render();
-}*/
-
-
-
-
-/*
-
-*/
