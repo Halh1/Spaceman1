5cfba58a5ec90d97e3615bf68eb97de3c617d89d
Hafsa <ali.hafsa.ahmed@gmail.com>
2024-03-21 10:56:07 -0500

    Loss logic and img implemented

--- js/main.js
+++ js/main.js
@@ -9,12 +9,14 @@ let entered; //tracking what is being entered?
 let correctLetters; // track if they are winning or not
 let wrongLetters; // tracking letters that are wrong
   // want to track how many hints they use up
+let currentWord;
 let winner; //tracking winnings 
+let loss;
 
 
 
 	/*----- cached elements  -----*/
-let imgEl = document.getElementById("body");
+let imgEl = document.getElementById("image");
 let messageEl = document.getElementById("status");
 let displayWord = document.getElementById("display");
 let letterEl = document.getElementById("keyboard");
@@ -35,34 +37,61 @@ init();
 function init() {
     correctLetters = KEYWORD_LOOKUP[Math.floor(Math.random() * KEYWORD_LOOKUP.length)].split(''); // should I randomize the hint and the keywords together?
     displayWord.innerText = correctLetters.map((letter)=> letter === "" ? "" : "_").join(" ");
+    currentWord = [];
     wrongLetters = [];
     hint = []; //Trying to find away to connect the randomization of correctLetters with it's matching hint.
+    hintEl.style.display = "none";
+    imgEl.style.display ="none";
     messageEl.innerText = "Guess!";
-    render();
+    loss = null;
+
+   // render();
 }
 
 
 
 function handleClick(event) {
     const key = event.target.textContent
-   // if (event.target.tagName === "BUTTON");
+    if (event.target.tagName === "BUTTON");
     //console.log(key);
     if(correctLetters.includes(key)) {
         correctLetters.forEach((char, idx) => {
-            if (char === key) displayWord[idx]= key;
+            if (char === key) displayWord[idx].innerText = key;
+       
         });
         } else {
             wrongLetters.push(key);
         }  
         alert(correctLetters); 
         alert(wrongLetters);
-    render();
-   
+        loss = checkLoss();
+        render();
 }
+function checkLoss() {
+    if (wrongLetters.length < tries) {
+        messageEl.innerText = "Try again";
+        imgEl.src = `img/Scooby-Doo${wrongLetters.length}.jpg`;
+        imgEl.style.display = "block";
+    } else {
+        messageEl.innerText = "Game Over!";
+        imgEl.src = `img/Scooby-Doo 0.jpg`;
+    }
+
+    
+} 
+
+function checkWin() {
+    if (currentWord.length === correctLetters.length) messageEl.innerText = "You've won!";
+} // not working b/c i need to fix array
 
 
- 
+//function renderImg(){
+    //imgEl.src = `img/Scooby-Doo ${wrongLetters.length}.jpg`;
+//}
 
- function render() {
 
-}
+//function for which hint, if correctletter === keylook[0] then hint === hintlook[0]
+// function render() {
+  //  imgEl.src = `img/Scooby-Doo ${wrongLetters.length - tries}.jpg`;
+
+//} 
